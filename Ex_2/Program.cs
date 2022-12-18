cb9c8d9e3c1d8867deb9d75981ddcc48b69b2a26
NikolaiB <altnikolos95@gmail.com>
2022-12-18 12:00:33 +0300

    з

--- Ex_2/Program.cs
+++ Ex_2/Program.cs
@@ -5,8 +5,15 @@
         public int count20;
         public int count50;
         public int count100;
-
-      
+        public int result;
+        public int money;
+        public ATM(int count20, int count50, int count100)
+        {
+            this.count20 = count20;
+            this.count50 = count50;
+            this.count100 = count100;
+            
+        }
 
         public void AddMoney(int count20, int count50, int count100)
         {
@@ -14,12 +21,26 @@
             this.count50 += count50;
             this.count100 += count100;
         }
-
-       
-
         public int MoneyinAtm()
         {
-            return count100 * 100 + count50 * 50 + count20 * 20;
+            int result = count100 * 100 + count50 * 50 + count20 * 20;
+            return result;
+        }
+
+        public void CashOut(int money)
+        {
+            if (money > MoneyinAtm())
+            {
+                Console.WriteLine("Not enought money");
+
+            }
+            else
+            {
+                Console.WriteLine("Done!");
+                
+
+            }
+
         }
 
 
@@ -32,10 +53,16 @@
     {
         static void Main(string[] args)
         {
-            ATM atm = new ATM();
-            atm.AddMoney(2, 3, 5);
+            ATM atm = new ATM(1,2,3);
+            atm.AddMoney(2, 3, 20);  
+            Console.WriteLine("Total : \t" + atm.MoneyinAtm());
+            Console.WriteLine("How much do you want cashout? ");
+            atm.CashOut(Convert.ToInt32(Console.ReadLine()));
             
-            
+           
+
+
+
         }
     }
 }
